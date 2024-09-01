ee5ee9a56c1d93eb93c3fe730b43ae0dfb715052
KarimovMurodilla <karimovmurodilla15@gmail.com>
2024-09-01 14:40:17 +0500

    solved linked-list-cycle problem using Floyd’s Tortoise and Hare algorithm

---
+++ linked-list/linked_list_cycle.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                return True
+            
+        return False
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+
+s = Solution()
+res = s.hasCycle(head)
+print(res)
